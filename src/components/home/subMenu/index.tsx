16f22e7b58ffbf78e40d1b576a6f90c7b9df0790
Deyvid silva <silvadeyvid017@gmail.com>
2024-08-19 22:33:22 -0300

    subindo o projeto

---
+++ src/components/home/subMenu/index.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import styles from "./stles.module.scss";
+
+import { X, Menu } from "lucide-react";
+
+export function SubMenu() {
+  return (
+    <section className={styles.submenu}>
+      <div className={styles.submenuIcon}>
+        <Menu size={34} color="#121212" />
+        Menu
+      </div>
+
+      <ul className={styles.ul}>
+        <li>
+          <Link href="/post/pagina-1">Pagina 1</Link>
+        </li>
+
+        <li>
+          <Link href="/post/pagina-2">Pagina 2</Link>
+        </li>
+      </ul>
+    </section>
+  );
+}
