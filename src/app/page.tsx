c4c3607885caa127f3018a3a9c8b3c6955e0a20f
Deyvid silva <silvadeyvid017@gmail.com>
2024-08-22 17:17:31 -0300

    adicionando about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
 import styles from "./page.module.scss";
 
+import { Container } from "@/components/container";
+import { Services } from "@/components/services";
 import { SubMenu } from "@/components/home/subMenu";
-
 import { getDataHome } from "@/utils/actions/get-data";
 import { HomeProps } from "@/utils/home.type";
+import { Hero } from "@/components/hero";
+import { Phone } from "lucide-react";
 
 export default async function Home() {
   const { object }: HomeProps = await getDataHome();
   console.log(object.title);
+
   return (
     <main>
       <SubMenu />
+      <Hero
+        heading={object.metadata.heading}
+        buttonTitle={object.metadata.cta_button.title}
+        buttonUrl={object.metadata.cta_button.url}
+        bannerUrl={object.metadata.banner.url}
+        icon={<Phone size={24} color="#fff" />}
+      />
+
+      <Container>
+        <Services object={object} />
+      </Container>
     </main>
   );
 }
